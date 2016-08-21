983b5a85f2438df7a1879c3fb14494990d34f7e3
Your NameAlexandr <shev_183@mail.ru>
2016-08-21 21:59:21 +0300

    jsHomeWork_1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+function pow(x, n) {
+	
+  var x = +prompt('Введите основание степени', '');
+  var n = +prompt('Введите показатель степени', '');
+  var result = x;
+
+    if (n > 0) {
+      for (var i = 1; i < n; i++) {
+        result *= x;
+      }
+      console.log('Результат равен  ' + result);
+      return result;
+    } else if (n === 0) {
+    	result = 1;
+  	    console.log('Результат равен  ' + result);
+        return 1;
+    } else if (n < 0) {
+    	for (i = -1; i > n; i--){
+    	result *= x;
+      }
+      result = 1 / result;
+        console.log('Результат равен  ' + result);
+        return result;
+    }
+}
+
+
+pow() ;
+
+
